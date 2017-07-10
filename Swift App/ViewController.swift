19560401d7cabb88dbfaf1e05beb9669a1477ef4
Vilde Kolberg <vildekolberg@vildes-macbook-air-4.local>
2017-07-10 23:49:35 +0200

    Initial Commit

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Vilde Kolberg on 10.07.2017.
+//  Copyright © 2017 SverrirApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
